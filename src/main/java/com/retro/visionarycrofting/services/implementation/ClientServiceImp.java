7693f4d248805355ff013aa37cb0de0aed61ba0a
anas jabellah <jablcodiing@gmail.com>
2022-11-28 19:07:50 +0100

    add client

--- src/main/java/com/retro/visionarycrofting/services/implementation/ClientServiceImp.java
+++ src/main/java/com/retro/visionarycrofting/services/implementation/ClientServiceImp.java
@@ -1,6 +1,11 @@
 package com.retro.visionarycrofting.services.implementation;
 
+import com.retro.visionarycrofting.repositories.ClientRepository;
 import com.retro.visionarycrofting.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ClientServiceImp implements ClientService {
+
 }

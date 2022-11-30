54969fc3c716549c60890a9246cfacf8ff4dd101
said hasnaoui <saidhasnaoui.uce@gmail.com>
2022-11-30 10:38:13 +0100

    create implimentation service

--- src/main/java/com/retro/visionarycrofting/services/StockService.java
+++ src/main/java/com/retro/visionarycrofting/services/StockService.java
@@ -1,65 +1,24 @@
 package com.retro.visionarycrofting.services;
 
-import com.retro.visionarycrofting.entities.Product;
 import com.retro.visionarycrofting.entities.Stock;
-import com.retro.visionarycrofting.repositories.StockDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 import java.util.List;
 
-@Service
-public class StockService {
-    @Autowired
-    StockDao stockDao;
-    @Autowired
-    ProductService productService;
+public interface StockService {
 
-    public List<Stock> findAllByAddress(String address) {
-        return stockDao.findAllByAddress(address);
-    }
+    public List<Stock> findAllByAddress(String address);
 
-    public Stock findByName(String name) {
-        return stockDao.findByName(name);
-    }
+    public Stock findByName(String name);
 
-    public Stock findByEmail(String email) {
-        return stockDao.findByEmail(email);
-    }
+    public Stock findByEmail(String email);
 
-    @Transactional
-    public int deleteByName(String name) {
-        return stockDao.deleteByName(name);
-    }
+    public int deleteByName(String name);
 
-    @Transactional
-    public int deleteByEmail(String email) {
-        return stockDao.deleteByEmail(email);
-    }
+    public int deleteByEmail(String email);
 
-    public List<Stock> findAll() {
-        return stockDao.findAll();
-    }
+    public List<Stock> findAll();
 
-    @Deprecated
-    public Stock getOne(Long aLong) {
-        return stockDao.getOne(aLong);
-    }
+    public Stock getOne(Long aLong);
 
-    public Stock save(Stock s) {
-        if (this.findByName(s.getName()) != null) return null;
-        if (this.findByEmail(s.getEmail()) != null) return null;
-        Stock stock = stockDao.save(s);
-        for (Product product: s.getProducts()) {
-            productService.save(product, stock.getName());
-        }
-        return stock;
-    }
-    public Stock update(Stock stock) {
-        if (this.findByName(stock.getName()) == null) return null;
-        if (this.findByEmail(stock.getEmail()) == null) return null;
-        if (this.getOne(stock.getId()) == null) return null;
-        return stockDao.save(stock);
-    }
+    public Stock save(Stock s);
+    public Stock update(Stock stock);
 }
